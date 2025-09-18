8d88a4e58d90fd3217c8c7dd65b0a3ffa841e4c7
Daniel Arvidsson <crowstudio@gmail.com>
2025-09-18 21:26:29 +0100

    Update tile comment for Zone

--- game_board/basic_tile.py
+++ game_board/basic_tile.py
@@ -7,20 +7,20 @@ class BasicTile:
     HEIGHT_OFFSET   = 40                                # +40 pixels to compensate for info bar at top
     BOARD_HEIGHT    = (SIZE * NUM_ROWS) + HEIGHT_OFFSET # Default 600 + 40
 
-    # Zone (01-99),                 Element number (00-99),                 False(NA)/True (00-01),             Element Type (00-99)
-    # 00 = basic elements           To keep track of multiple instanses     NA = no states, passive element     00 = START
-    #                                                                                                           01 = PIT1
-    #                                                                                                           02 = PIT2
-    #                                                                                                           03 = PIT3
-    #                                                                                                           04 = PIT4
-    #                                                                                                           05 = BOTTOMLESS_PIT
-    #                                                                                                           06 = WALL
-    #                                                                                                           07 = FLOOR
-    #                                                                                                           08 = EXIT
-    #                                                                                                           09 = WALL_SWITCH_*
-    #                                                                                                           10 = FLOOR_SWITCH_*
-    #                                                                                                           11 = TRAP_DOOR_*
-    #                                                                                                           12 = SLIDING_DOOR_*
+    # Zone (101-199),           Element number (00-99),                 False(NA)/True (00-01),             Element Type (00-99)
+    # 101 = basic elements      To keep track of multiple instanses     NA = no states, passive element     00 = START
+    #                                                                                                       01 = PIT1
+    #                                                                                                       02 = PIT2
+    #                                                                                                       03 = PIT3
+    #                                                                                                       04 = PIT4
+    #                                                                                                       05 = BOTTOMLESS_PIT
+    #                                                                                                       06 = WALL
+    #                                                                                                       07 = FLOOR
+    #                                                                                                       08 = EXIT
+    #                                                                                                       09 = WALL_SWITCH_*
+    #                                                                                                       10 = FLOOR_SWITCH_*
+    #                                                                                                       11 = TRAP_DOOR_*
+    #                                                                                                       12 = SLIDING_DOOR_*
 
     START       = 101000000
     PIT1        = 101010101
